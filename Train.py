6aa09d08a2b6983b9760e154aac448f0b93ccf85
Marcob1996 <marcob@umd.edu>
2022-10-04 14:59:39 -0400

    Create sparse training

--- Train.py
+++ Train.py
@@ -3,35 +3,60 @@ import tensorflow as tf
 from mlp import SparseNeuralNetwork
 from dataloader import load_amazon670
 from bce_loss import custom_bce
+import time
 import os
 
 
+def train(train_data, test_data):
 
+    layer_dims = [135909, 128, 670091]
+    # 670091
+    model = SparseNeuralNetwork(layer_dims)
+    optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
 
-def train():
-    layer_dims = [100, 128, 10]
-    nn = SparseNeuralNetwork(layer_dims)
+    epochs = 2
+    for epoch in range(epochs):
+        print("\nStart of epoch %d" % (epoch,))
 
-    sparse_data = tf.sparse.SparseTensor(
-        indices=[(0, 0), (0, 1), (0, 2),
-                 (4, 3), (5, 0), (5, 1)],
-        values=[1, 1, 1, 1, 1, 1],
-        dense_shape=(10, 100)
-    )
+        # Iterate over the batches of the dataset.
+        for step, (x_batch_train, y_batch_train) in enumerate(train_data):
 
-    sparse_y = tf.sparse.SparseTensor(
-        indices=[(0, 0), (0, 1), (0, 2),
-                 (4, 3), (5, 0), (5, 1)],
-        values=[1., 1., 1., 1., 1., 1.],
-        dense_shape=(10, 10),
-    )
+            t = time.time()
+            # Open a GradientTape to record the operations run
+            # during the forward pass, which enables auto-differentiation.
+            with tf.GradientTape() as tape:
 
-    a = nn(sparse_data)
-    loss = custom_bce(sparse_y, a)
-    print(loss)
+                # Run the forward pass of the layer.
+                # The operations that the layer applies
+                # to its inputs are going to be recorded
+                # on the GradientTape.
+                logits = model(x_batch_train, training=True)  # Logits for this minibatch
 
+                # Compute the loss value for this minibatch.
+                loss_value = custom_bce(y_batch_train, logits)
+
+            # Use the gradient tape to automatically retrieve
+            # the gradients of the trainable variables with respect to the loss.
+            grads = tape.gradient(loss_value, model.trainable_weights)
+
+            # Run one step of gradient descent by updating
+            # the value of the variables to minimize the loss.
+            optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+            print('Step Finished in %f Seconds' % (time.time() - t))
+
+            # Log every 200 batches.
+            if step % 5 == 0:
+                print(
+                    "Training loss (for one batch) at step %d: %.4f"
+                    % (step, float(loss_value))
+                )
+                print("Seen so far: %s samples" % ((step + 1) * batch_size))
 
 
 if __name__ == '__main__':
-    # load_amazon670()
-    train()
+    batch_size = 16
+    print('Loading data...')
+    train_data, test_data = load_amazon670(batch_size)
+    print('Beginning training...')
+    train(train_data, test_data)
--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,23 @@
 import numpy as np
+import tensorflow as tf
 from xclib.data import data_utils
 
 
-def load_amazon670():
-    print('hi')
+def convert_sparse_matrix_to_sparse_tensor(X):
+    coo = X.tocoo()
+    indices = np.mat([coo.row, coo.col]).transpose()
+    return tf.sparse.reorder(tf.SparseTensor(indices, coo.data, coo.shape))
+
+
+def load_amazon670(batch_size):
     features, labels, num_samples, num_features, num_labels = data_utils.read_data('Data/Amazon670k/train.txt')
+    features_t, labels_t, num_labels_t, num_features_t, num_labels_t = data_utils.read_data('Data/Amazon670k/test.txt')
+    # Create sparse tensors
+    trn = convert_sparse_matrix_to_sparse_tensor(features)
+    tst = convert_sparse_matrix_to_sparse_tensor(features_t)
+    trn_labels = convert_sparse_matrix_to_sparse_tensor(labels)
+    tst_labels = convert_sparse_matrix_to_sparse_tensor(labels_t)
+    # Create train and test datasets.
+    trn_dataset = tf.data.Dataset.from_tensor_slices((trn, trn_labels)).batch(batch_size)
+    tst_dataset = tf.data.Dataset.from_tensor_slices((tst, tst_labels)).batch(batch_size)
+    return trn_dataset, tst_dataset
