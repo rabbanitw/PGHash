Further fix bce to overcome ("cannot track gradients through object assignments") issue. Now training runs smoothly

Train.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-# import tensorflow_datasets as tfds
 from mlp import SparseNeuralNetwork
 from dataloader import load_amazon670
 from bce_loss import sparse_bce
@@ -11,7 +10,6 @@ def train(train_data, test_data):
 
     layer_dims = [135909, 128, 670091]
     model = SparseNeuralNetwork(layer_dims)
-    model.summary()
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
 
     epochs = 2
@@ -30,10 +28,10 @@ def train(train_data, test_data):
                 # The operations that the layer applies
                 # to its inputs are going to be recorded
                 # on the GradientTape.
-                logits = model(x_batch_train, training=True)  # Logits for this minibatch
+                y_pred = model(x_batch_train, training=True)
 
                 # Compute the loss value for this minibatch.
-                loss_value = sparse_bce(y_batch_train, logits)
+                loss_value = sparse_bce(y_batch_train, y_pred)
 
             # Use the gradient tape to automatically retrieve
             # the gradients of the trainable variables with respect to the loss.