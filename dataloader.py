e2a7eff3145ed4af02b3b92de9ebb1e4c2387dd8
Marcob1996 <marcob@umd.edu>
2022-10-06 17:23:27 -0400

    Add data partioning

--- dataloader.py
+++ dataloader.py
@@ -9,15 +9,40 @@ def convert_sparse_matrix_to_sparse_tensor(X):
     return tf.sparse.reorder(tf.SparseTensor(indices, coo.data, coo.shape))
 
 
-def load_amazon670(batch_size):
+def load_amazon670(rank, size, batch_size):
+
+    # load data
     features, labels, num_samples, num_features, num_labels = data_utils.read_data('Data/Amazon670k/train.txt')
     features_t, labels_t, num_labels_t, num_features_t, num_labels_t = data_utils.read_data('Data/Amazon670k/test.txt')
+
+    # partition data amongst workers
+    worker_features = partition_sparse_dataset(features, rank, size)
+    worker_features_t = partition_sparse_dataset(features_t, rank, size)
+    worker_labels = partition_sparse_dataset(labels, rank, size)
+    worker_labels_t = partition_sparse_dataset(labels_t, rank, size)
+
     # Create sparse tensors
-    trn = convert_sparse_matrix_to_sparse_tensor(features)
-    tst = convert_sparse_matrix_to_sparse_tensor(features_t)
-    trn_labels = convert_sparse_matrix_to_sparse_tensor(labels)
-    tst_labels = convert_sparse_matrix_to_sparse_tensor(labels_t)
-    # Create train and test datasets.
+    trn = convert_sparse_matrix_to_sparse_tensor(worker_features)
+    tst = convert_sparse_matrix_to_sparse_tensor(worker_features_t)
+    trn_labels = convert_sparse_matrix_to_sparse_tensor(worker_labels)
+    tst_labels = convert_sparse_matrix_to_sparse_tensor(worker_labels_t)
+
+    # Create train and test datasets
     trn_dataset = tf.data.Dataset.from_tensor_slices((trn, trn_labels)).batch(batch_size)
     tst_dataset = tf.data.Dataset.from_tensor_slices((tst, tst_labels)).batch(batch_size)
     return trn_dataset, tst_dataset
+
+
+def partition_sparse_dataset(data_array, rank, size, partition_type='Simple'):
+    if partition_type == 'Simple':
+        r, c = data_array.shape
+        start_idx = rank*int(r/size)
+        end_idx = (rank+1)*int(r/size)
+        if rank == size - 1:
+            end_idx = r
+        worker_data = data_array[start_idx:end_idx, :]
+        return worker_data
+
+    # To Do: implement other partition methods below...
+    else:
+        return np.array_split(data_array, size)[rank]
--- run.py
+++ run.py
@@ -25,6 +25,8 @@ if __name__ == '__main__':
     layer_dims = [135909, 128, 670091]
     model = SparseNeuralNetwork(layer_dims)
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    batch_size = 64
+    epochs = 2
 
     # get model architecture
     layer_shapes, layer_sizes = get_model_architecture(model)
@@ -32,9 +34,8 @@ if __name__ == '__main__':
     # initialize D-SGD
     communicator = DecentralizedSGD(rank, size, MPI.COMM_WORLD, G, layer_shapes, layer_sizes, 0, 1)
 
-    batch_size = 64
-    epochs = 2
-    print('Loading data...')
-    train_data, test_data = load_amazon670(batch_size)
+    print('Loading and partitioning data...')
+    train_data, test_data = load_amazon670(rank, size, batch_size)
+
     print('Beginning training...')
     train(model, optimizer, train_data, test_data, epochs)
