bc7b288f3c63cc09465aef22a3972727abc0b8e5
Tahseen Rabbani <49704573+rabbanitw@users.noreply.github.com>
2022-10-09 02:27:40 -0400

    Update lsh.py
    Included two different sampling options for neurons.

--- lsh.py
+++ lsh.py
@@ -1,108 +1,154 @@
 import numpy as np
+import random
 import tensorflow as tf
 
 #Takes a layer input and determines which weights are cosin (dis)similar via PGHash
 #Parameters:
-#in_layer = layer input
+#in_layer = layer input, must be a column-vector
 #weight = weight tensor of current layer
 #sdim = length of hash signature. must divide length of in_layer/# of weight rows
 #num_tables = how many hash tables to compare across
 #cr = compression rate, percentage of rows of weight matrix to preserve
-#RETURNS: row indices
-def pglayer(in_layer,weight, sdim, num_tables, cr):
-         
-    #Instead of a normal random vector, it's going to be a repeating Gaussian vector.
-    def pghash(vec1, vec2,n, sdim):
-        sig1=np.zeros(sdim)
-        sig2=np.zeros(sdim)
-        for i in range(sdim):
-            rand_vec=(1/np.sqrt(int(sdim)))*tf.random.normal([sdim])
-            pg_vec=rand_vec
-            for _ in range(int(n/sdim)-1):
-                pg_vec=tf.concat([pg_vec,rand_vec],0)
-            #If sdim does not evenly divide n:
-            if int(n/sdim)*sdim != int(n):
-                diff=n-int(n/sdim)*sdim
-                pg_vec=np.concatenate([pg_vec,rand_vec[:diff]],0)
-            sign1=np.sign(np.dot(pg_vec,vec1))
-            sign2=np.sign(np.dot(pg_vec,vec2))
-            if sign1<=0:
-                sign1=0
-            if sign2<=0:
-                sign2=0
-            sig1[i]=sign1
-            sig2[i]=sign2
-        return np.count_nonzero(sig1!=sig2)
+def pg_vanilla(in_layer,weight, sdim, num_tables, cr):
     
-    #Calculates PGHash similarities between a vector and a list.
-    def pg_tables(v,vectors, n, sdim, num_tables):
-        ham_avgs=[]
-        #Loop over weights
-        for i in range(vectors.shape[0]):
-            hamming_dists=[]
-            for _ in range(num_tables):
-                hamming_dists.append(pghash(v,vectors[i], weight.shape[1], sdim))
-            ham_avgs.append(np.average(hamming_dists))
-        return np.asarray(ham_avgs)
-    
-    #Get the # of indices we'd like to keep.
-    rows=weight.shape[0]
-    num_inds = int(rows*cr)
-    ham_avgs=pg_tables(in_layer, weight, weight.shape[1], sdim, num_tables)
-    #Get vectors with high hamming distance (far angles)
-    close_inds = (tf.math.top_k(ham_avgs,int(num_inds/2))).indices.numpy()
-    #Get vectors with low hamming distance (close angles)
-    far_inds= (tf.math.top_k(-1*ham_avgs,int(num_inds/2))).indices.numpy()
-    inds = list(close_inds)+list(far_inds)
+    #Parameters:
+    #limit = total number of required neurons
+    def pghash(vec1, vectors, n, sdim):
+        match_indices=[]
+        pg_mat=np.eye(sdim)
+        #Build PG_Hash Matrix, rows look like repeating Gaussians
+        for i in range(int(np.ceil(n/sdim))):
+            pg_mat=np.concatenate((pg_mat,np.eye(sdim)),1)
+        rand_gauss=np.random.normal(size=(sdim,sdim))
+        pg_mat=(1/int(n/sdim))*rand_gauss@pg_mat[:,:n]
+        #Apply PGHash to input vector.
+        sig1=np.heaviside(pg_mat@vec1,0)
+        for j in range(weight.shape[0]):
+            vec2=weight[j]
+            sig2=np.heaviside(pg_mat@vec2,0)
+            #Matching hash signatures
+            if np.array_equal(sig1,sig2):
+                match_indices.append(j)
+        return match_indices
 
+    n=weight.shape[1]
+    thresh=int(cr*weight.shape[0])
+    inds=[]
+    #Loop over the desired number of tables.
+    for _ in range(num_tables):
+        inds += pghash(in_layer, weight, n, sdim)
+        inds=list(set(inds))
+        if len(inds)>thresh:
+            return random.sample(inds,k=thresh)
     return inds
 
 
-#Takes a layer input and determines which weights are close/far in angle via SLIDE
+#Takes a layer input and determines which weights are cosin (dis)similar via PGHash
 #Parameters:
-#in_layer = layer input
+#in_layer = layer input, must be a column-vector
 #weight = weight tensor of current layer
 #sdim = length of hash signature. must divide length of in_layer/# of weight rows
 #num_tables = how many hash tables to compare across
 #cr = compression rate, percentage of rows of weight matrix to preserve
-#RETURNS: row indices
-def slidelayer(in_layer,weight, sdim, num_tables, cr):
-         
-    #Instead of a normal random vector, it's going to be a repeating Gaussian vector.
-    def slidehash(vec1, vec2, n, sdim):
-        sig1=np.zeros(sdim)
-        sig2=np.zeros(sdim)
-        for i in range(sdim):
-            rand_vec=(1/np.sqrt(int(sdim)))*tf.random.normal([n])
-            sign1=np.sign(np.dot(rand_vec,vec1))
-            sign2=np.sign(np.dot(rand_vec,vec2))
-            if sign1<=0:
-                sign1=0
-            if sign2<=0:
-                sign2=0
-            sig1[i]=sign1
-            sig2[i]=sign2
-        return np.count_nonzero(sig1!=sig2)
+def slide_vanilla(in_layer,weight, sdim, num_tables, cr):
     
-    #Calculates PGHash similarities between a vector and a list.
-    def slide_tables(v,vectors, n, sdim, num_tables):
-        ham_avgs=[]
-        #Loop over weights
-        for i in range(vectors.shape[0]):
-            hamming_dists=[]
-            for _ in range(num_tables):
-                hamming_dists.append(slidehash(v,vectors[i], weight.shape[1], sdim))
-            ham_avgs.append(np.average(hamming_dists))
-        return np.asarray(ham_avgs)
-    
-    #Get the # of indices we'd like to keep.
-    rows=weight.shape[0]
-    num_inds = int(rows*cr)
-    ham_avgs=slide_tables(in_layer, weight, weight.shape[1], sdim, num_tables)
-    #Get vectors with high hamming distance (far angles)
-    close_inds = (tf.math.top_k(ham_avgs,int(num_inds/2))).indices.numpy()
-    #Get vectors with low hamming distance (close angles)
-    far_inds= (tf.math.top_k(-1*ham_avgs,int(num_inds/2))).indices.numpy()
-    inds = list(close_inds)+list(far_inds)
+    #Parameters:
+    #limit = total number of required neurons
+    def slidehash(vec1, vectors, n, sdim):
+        match_indices=[]
+        slide_mat=np.random.normal(size=(sdim,n))
+        #Apply PGHash to input vector.
+        sig1=np.heaviside(slide_mat@vec1,0)
+        for j in range(weight.shape[0]):
+            vec2=weight[j]
+            sig2=np.heaviside(slide_mat@vec2,0)
+            #Matching hash signatures
+            if np.array_equal(sig1,sig2):
+                match_indices.append(j)
+        return match_indices
 
+    n=weight.shape[1]
+    thresh=int(cr*weight.shape[0])
+    inds=[]
+    #Loop over the desired number of tables.
+    for _ in range(num_tables):
+        inds += slidehash(in_layer, weight, n, sdim)
+        inds = list(set(inds))
+        if len(inds)>thresh:
+            return random.sample(inds,k=thresh)
+    return inds
+    
+
+
+#Takes a layer input and determines which weights are cosine (dis)similar via PGHash
+#Parameters:
+#in_layer = layer input, must be a column-vector
+#weight = weight tensor of current layer
+#sdim = length of hash signature. must divide length of in_layer/# of weight rows
+#num_tables = how many hash tables to compare across
+#cr = compression rate, percentage of rows of weight matrix to preserve
+def pg_avg(in_layer, weight, sdim, num_tables, cr):
+    
+    #Parameters:
+    #limit = total number of required neurons
+    def pghash(vec1, vectors, n, sdim):
+        dists=np.zeros(vectors.shape[0])
+        pg_mat=np.eye(sdim)
+        #Build PG_Hash Matrix, rows look like repeating Gaussians
+        for i in range(int(np.ceil(n/sdim))):
+            pg_mat=np.concatenate((pg_mat,np.eye(sdim)),1)
+        rand_gauss=np.random.normal(size=(sdim,sdim))
+        pg_mat=(1/int(n/sdim))*rand_gauss@pg_mat[:,:n]
+        #Apply PGHash to input vector.
+        sig1=np.heaviside(pg_mat@vec1,0)
+        for j in range(weight.shape[0]):
+            vec2=weight[j]
+            sig2=np.heaviside(pg_mat@vec2,0)
+            #Hamming distances between signatures
+            dists[j]=(np.count_nonzero(sig1!=sig2))
+        return dists
+
+    n=weight.shape[1]
+    rows=weight.shape[0]
+    ham_dists=np.zeros(rows)
+    thresh=int(rows*cr)
+    #Loop over the desired number of tables.
+    for _ in range(num_tables):
+        ham_dists += pghash(in_layer, weight, n, sdim)
+    inds=(tf.math.top_k((-1/num_tables)*ham_dists,thresh)).indices.numpy()
+    return inds
+
+
+#Takes a layer input and determines which weights are cosine (dis)similar via PGHash
+#Parameters:
+#in_layer = layer input, must be a column-vector
+#weight = weight tensor of current layer
+#sdim = length of hash signature. must divide length of in_layer/# of weight rows
+#num_tables = how many hash tables to compare across
+#cr = compression rate, percentage of rows of weight matrix to preserve
+def slide_avg(in_layer,weight, sdim, num_tables, cr):
+    
+    #Parameters:
+    #limit = total number of required neurons
+    def slidehash(vec1, vectors, n, sdim):
+        dists=np.zeros(vectors.shape[0])
+        #Build random Gaussian matrix
+        slide_mat=np.random.normal(size=(sdim, n))
+        #Apply PGHash to input vector.
+        sig1=np.heaviside(slide_mat@vec1,0)
+        for j in range(weight.shape[0]):
+            vec2=weight[j]
+            sig2=np.heaviside(slide_mat@vec2,0)
+            #Hamming distances between signatures
+            dists[j]=(np.count_nonzero(sig1!=sig2))
+        return dists
+
+    n=weight.shape[1]
+    rows=weight.shape[0]
+    ham_dists=np.zeros(rows)
+    thresh=int(rows*cr)
+    #Loop over the desired number of tables.
+    for _ in range(num_tables):
+        ham_dists += slidehash(in_layer, weight, n, sdim)
+    inds=(tf.math.top_k((-1/num_tables)*ham_dists,thresh)).indices.numpy()
     return inds
