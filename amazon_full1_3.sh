590ff62b1712b8a0d6987b763f2a28f386fb0b28
Marcob1996 <marcob@umd.edu>
2023-01-19 21:05:37 -0500

    Add Fedavg Amazon

--- amazon_full1_3.sh
+++ amazon_full1_3.sh
@@ -2,7 +2,7 @@
 
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
-#SBATCH --job-name=amz_f    # sets the job name if not set from environment
+#SBATCH --job-name=amz_f1_3    # sets the job name if not set from environment
 #SBATCH --time=26:00:00    # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --account=scavenger    # set QOS, this will determine what resources can be requested
 #SBATCH --qos=scavenger    # set QOS, this will determine what resources can be requested
@@ -17,4 +17,4 @@ module load mpi
 module load cuda/11.4.4
 source ../../../../cmlscratch/marcob/environments/pghash/bin/activate
 
-mpirun -n 1 python run_pg.py --hash_type regular --steps_per_test 500 --train_bs 256 --sdim 8 --dataset Amazon670K --cr 1 --name test1 --randomSeed 1203
+mpirun -n 1 python run_pg.py --hash_type regular --steps_per_test 500 --train_bs 256 --sdim 8 --dataset Amazon670K --cr 1 --name test3 --randomSeed 5425
