50de4b0c41346f6c4f7d46db08faac3648958ae5
Marcob1996 <marcob@umd.edu>
2023-01-25 19:32:47 -0500

    Delicious Batch Scripts

--- pg_del1_0.1-50t.sh
+++ pg_del1_0.1-50t.sh
@@ -2,7 +2,7 @@
 
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
-#SBATCH --job-name=p1d0.5    # sets the job name if not set from environment
+#SBATCH --job-name=p1d0.1-50t    # sets the job name if not set from environment
 #SBATCH --time=24:00:00    # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --account=scavenger    # set QOS, this will determine what resources can be requested
 #SBATCH --qos=scavenger    # set QOS, this will determine what resources can be requested
@@ -11,11 +11,11 @@
 #SBATCH --mem 128gb         # memory required by job; if unit is not specified MB will be assumed
 #SBATCH --nice=0
 #SBATCH --mail-type=END   # Valid type values are NONE, BEGIN, END, FAIL, REQUEUE
-#SBATCH --nodelist=cml11
+#SBATCH --nodelist=cml05
 
 module purge
 module load mpi
 module load cuda/11.4.4
 source ../../../../cmlscratch/marcob/environments/pghash/bin/activate
 
-mpirun -n 1 python run_pg.py --hash_type pghash --steps_per_test 100 --train_bs 128 --dataset Delicious200K --cr 0.5 --q 1 --name exp-t1 --randomSeed 1203
+mpirun -n 1 python run_pg.py --hash_type pghash --steps_per_test 100 --train_bs 128 --dataset Delicious200K --cr 0.1 --q 1 --name exp-t1-t50 --num_tables 50 --randomSeed 1203
--- pg_del8_0.1.sh
+++ pg_del8_0.1.sh
@@ -11,11 +11,11 @@
 #SBATCH --mem 128gb         # memory required by job; if unit is not specified MB will be assumed
 #SBATCH --nice=0
 #SBATCH --mail-type=END   # Valid type values are NONE, BEGIN, END, FAIL, REQUEUE
-#SBATCH --nodelist=cml01
+#SBATCH --nodelist=cml09
 
 module purge
 module load mpi
 module load cuda/11.4.4
 source ../../../../cmlscratch/marcob/environments/pghash/bin/activate
 
-mpirun -n 8 python run_pg.py --hash_type pghash --steps_per_test 100 --train_bs 128 --dataset Delicious200K --cr 0.1 --q 1 --name exp-t1 --randomSeed 1203
+mpirun -n 8 python run_pg.py --hash_type pghash --steps_per_test 100 --epochs 30 --train_bs 128 --dataset Delicious200K --cr 0.1 --q 1 --name exp-t1 --randomSeed 1203
