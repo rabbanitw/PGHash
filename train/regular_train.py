Fix Sampled Softmax and Send out Amazon Script

train/regular_train.py
@@ -37,7 +37,7 @@ def regular_train(rank, size, Method, optimizer, train_data, test_data, losses, 
                     for (x_batch_test, y_batch_test) in sub_test_data:
                         y_pred_test = model(x_batch_test, training=False)
                         test_acc1.update_state(y_pred_test, tf.sparse.to_dense(y_batch_test))
-                        test_acc = test_acc1.result().numpy()
+                    test_acc = test_acc1.result().numpy()
                     print("Step %d: Top 1 Test Accuracy %.4f" % (iterations-1, test_acc))
                     recorder.add_testacc(test_acc)
                     test_acc1.reset_state()

train/regular_train_ss.py
@@ -11,9 +11,9 @@ def regular_train_ss(rank, size, Method, optimizer, train_data, test_data, losse
     lsh_time = 0
     comm_time = 0
     iterations = 1
-    acc1 = 0
     num_final_layers = Method.nl*args.cr
-    full_size = np.arange(Method.nl-1)
+    acc1_metric = tf.keras.metrics.TopKCategoricalAccuracy(k=1)
+    test_acc1 = tf.keras.metrics.TopKCategoricalAccuracy(k=1)
 
     # update indices with new current index
     Method.ci = np.arange(Method.nl)
@@ -35,16 +35,13 @@ def regular_train_ss(rank, size, Method, optimizer, train_data, test_data, losse
                 if rank == 0:
                     test_data.shuffle(len(test_data))
                     sub_test_data = test_data.take(30)
-                    total_test_acc = 0
-                    examples = 0
                     for (x_batch_test, y_batch_test) in sub_test_data:
-                        test_acc = model((x_batch_test, y_batch_test), training=False)
-                        bs = x_batch_test.get_shape()[0]
-                        total_test_acc += test_acc * bs
-                        examples += bs
-                    ta = total_test_acc/examples
-                    print("Step %d: Top 1 Test Accuracy %.4f" % (iterations-1, ta))
-                    recorder.add_testacc(ta)
+                        y_pred_test = model.full_model(x_batch_test, training=False)
+                        test_acc1.update_state(y_pred_test, tf.sparse.to_dense(y_batch_test))
+                    test_acc = test_acc1.result().numpy()
+                    print("Step %d: Top 1 Test Accuracy %.4f" % (iterations - 1, test_acc))
+                    recorder.add_testacc(test_acc)
+                    test_acc1.reset_state()
 
             init_time = time.time()
 
@@ -55,25 +52,16 @@ def regular_train_ss(rank, size, Method, optimizer, train_data, test_data, losse
             # transform sparse label to dense sub-label
             batch = x_batch_train.get_shape()[0]
 
-            '''
-            y = tf.sparse.to_dense(y_batch_train).numpy()
-            nz = np.count_nonzero(y, axis=1)
-            max_num_labels = np.max(nz)
-            # nz_indices = np.empty((batch, max_num_labels))
-            nz_indices = np.ones((batch, max_num_labels)) * (Method.nl-1)
-            for i in range(y.shape[0]):
-                num_nz = nz[i]
-                y_row = y[i, :]
-                nz_indices[i, :num_nz] = full_size[y_row>0]
-                # if num_nz < max_num_labels:
-                #    nz_indices[i, num_nz:] = nz_indices[i, num_nz-1]
-            y_labels = tf.convert_to_tensor(nz_indices)
-            '''
+            y_true = tf.sparse.to_dense(y_batch_train)
+
+            # randomize the label selected (since there's a tie)
+            y_true_ind = tf.math.argmax(y_true*tf.random.uniform(tf.shape(y_true)), axis=1)
+            # y_true_ind = tf.math.argmax(y_true, axis=1)
 
             # perform gradient update
             with tf.GradientTape() as tape:
-                loss_value = model([x_batch_train, y_batch_train], training=True)
-                # loss_value = model([x_batch_train, y], training=True)
+                inp = model.half_model(x_batch_train, training=True)  # Forward pass
+                y_pred, loss_value = model.train_loss(y_true_ind, [inp, model.out_bias, model.out_weights])
 
             # apply backpropagation after setting non-active weights to zero
             grads = tape.gradient(loss_value, model.trainable_weights)
@@ -83,6 +71,8 @@ def regular_train_ss(rank, size, Method, optimizer, train_data, test_data, losse
             rec_init = time.time()
             losses.update(np.array(loss_value), batch)
             # accuracy calc
+            acc1_metric.update_state(y_pred, y_true)
+            acc1 = acc1_metric.result().numpy()
             top1.update(acc1, batch)
             record_time = time.time() - rec_init
             comp_time = (time.time() - init_time) - record_time
@@ -93,25 +83,25 @@ def regular_train_ss(rank, size, Method, optimizer, train_data, test_data, losse
             recorder.save_to_file()
 
             # log every X batches
+            acc1_metric.reset_state()
             total_batches += batch
             if iterations % 5 == 0:
                 print(
-                    "(Rank %d) Step %d: Epoch Time %f, Loss %.6f, [%d Total Samples]"
-                    % (rank, iterations, (comp_time+comm_time), loss_value.numpy(), total_batches)
+                    "(Rank %d) Step %d: Epoch Time %f, Loss %.6f, Top 1 Train Accuracy %.4f, [%d Total Samples]"
+                    % (rank, iterations, (comp_time + comm_time), loss_value.numpy(), acc1, total_batches)
                 )
 
             iterations += 1
 
-        total_test_acc = 0
-        examples = 0
-        for (x_batch_test, y_batch_test) in test_data:
-            test_acc = model((x_batch_test, y_batch_test), training=False)
-            bs = x_batch_test.get_shape()[0]
-            total_test_acc += test_acc * bs
-            examples += bs
-        ta = total_test_acc / examples
-        print("Step %d: Top 1 Test Accuracy %.4f" % (iterations - 1, ta))
-        recorder.add_testacc(ta)
+        if rank == 0:
+            for (x_batch_test, y_batch_test) in test_data:
+                y_pred_test = model.full_model(x_batch_test, training=False)
+                test_acc1.update_state(y_pred_test, tf.sparse.to_dense(y_batch_test))
+            test_acc = test_acc1.result().numpy()
+            print("Step %d: Top 1 Test Accuracy %.4f" % (iterations - 1, test_acc))
+            recorder.add_testacc(test_acc)
+            test_acc1.reset_state()
+
         # reset accuracy statistics for next epoch
         top1.reset()
         losses.reset()

util/mlp.py
@@ -1,70 +1,4 @@
 import tensorflow as tf
-import numpy as np
-
-
-class SS_Linear(tf.keras.layers.Layer):
-    def __init__(self, units, input_dim, num_classes, cr):
-        super().__init__()
-        self.num_classes = num_classes
-        self.num_sampled = int(num_classes * cr)
-        self.acc = tf.keras.metrics.TopKCategoricalAccuracy(k=1)
-        w_init = tf.random_normal_initializer()
-        self.w = tf.Variable(
-            initial_value=w_init(shape=(input_dim, units), dtype="float32"),
-            trainable=True,
-        )
-        b_init = tf.zeros_initializer()
-        self.b = tf.Variable(
-            initial_value=b_init(shape=(units,), dtype="float32"), trainable=True
-        )
-
-    def call(self, inputs, training=True):
-        (x, y) = inputs
-        if training:
-
-            y = tf.sparse.to_dense(y)
-            # randomize the label selected (since there's a tie)
-            # label_idx = tf.expand_dims(tf.math.argmax(y*tf.random.uniform(tf.shape(y)), axis=1), axis=1)
-            # regular argmax
-            label_idx = tf.expand_dims(tf.math.argmax(y, axis=1), axis=1)
-            loss = tf.reduce_mean(tf.nn.sampled_softmax_loss(weights=tf.transpose(self.w), biases=self.b, labels=label_idx,
-                                                             inputs=x, num_sampled=self.num_sampled,
-                                                             num_classes=self.num_classes,
-                                                             #num_true=y.get_shape()[1],
-                                                             remove_accidental_hits=False))
-            return loss
-        else:
-            logits = tf.matmul(x, self.w) + self.b
-            logits = tf.convert_to_tensor(logits.numpy()[:, :-1])
-            y = tf.sparse.to_dense(y)
-            self.acc.update_state(logits, y)
-            accuracy = self.acc.result().numpy()
-            self.acc.reset_state()
-            return accuracy
-
-class Sparse_Linear(tf.keras.layers.Layer):
-    def __init__(self, units=32, input_dim=32):
-        super(Sparse_Linear, self).__init__()
-        self.w = self.add_weight(
-            shape=(input_dim, units), initializer="random_normal", trainable=True
-        )
-        self.b = self.add_weight(shape=(units,), initializer="zeros", trainable=True)
-
-    def call(self, inputs):
-        return tf.sparse.sparse_dense_matmul(inputs, self.w) + self.b
-
-
-class SampledSoftmax(tf.keras.Model):
-
-    def __init__(self, layer_dims, cr=0.1, name="sampled_softmax", **kwargs):
-        super(SampledSoftmax, self).__init__(name=name, **kwargs)
-        self.sparse_dense = Sparse_Linear(layer_dims[1], layer_dims[0])
-        self.sampled_softmax_layer = SS_Linear(layer_dims[2], layer_dims[1], layer_dims[2], cr)
-
-    def call(self, inputs, training=True):
-        (x, y) = inputs
-        output = self.sparse_dense(x)
-        return self.sampled_softmax_layer([output, y], training=training)
 
 
 def DenseLayer(output_dim):
@@ -81,3 +15,77 @@ def SparseNeuralNetwork(layer_dims, sparsity=True):
     # return the constructed network architecture
     return model
 
+
+class SampledSoftmaxLoss:
+    """
+    Class that instantiates a sampled softmax loss
+    ...
+    Attributes
+    ----------
+    num_classes : int
+        Total number of classes in the softmax output
+    sample_frac : float
+        Fraction of num_classes to sample
+    num_true_labels: int
+        Number of true labels in the final output (1 for multiclass classfication)
+    loss_name: string
+        (Optional) Name for the loss function
+    Methods
+    -------
+    loss(y_true, data):
+        Returns the sampled softmax loss given the true label and the all additional data
+    """
+
+    def __init__(self, num_classes, num_sampled, num_true_labels=1, loss_name="sampled_softmax_loss"):
+        self.num_classes = num_classes
+        self.num_true_labels = num_true_labels
+        self.num_sampled = num_sampled
+        self.loss_name = loss_name
+
+    def loss(self, y_true, data):
+        """
+        inputs to the softmax layer
+        """
+        inp, bias, weights = data
+        if self.num_true_labels == 1:
+            labels = tf.expand_dims(y_true, -1)
+        else:
+            labels = y_true
+
+        logits = tf.matmul(inp, weights)
+        logits = tf.nn.bias_add(logits, bias)
+
+        return logits, tf.reduce_mean(tf.nn.sampled_softmax_loss(
+            weights=tf.transpose(weights),
+            biases=bias,
+            labels=labels,
+            inputs=inp,
+            num_true=self.num_true_labels,
+            num_sampled=self.num_sampled,
+            num_classes=self.num_classes,
+            remove_accidental_hits=True,
+            name=self.loss_name
+        ))
+
+
+class SampledSoftmax(tf.keras.Model):
+
+    def __init__(self, layer_dims, num_sampled):
+        super().__init__()
+
+        self.input_size = layer_dims[0]
+        self.hls = layer_dims[1]
+        self.num_classes = layer_dims[2]
+
+        inp = tf.keras.layers.Input(shape=(self.input_size,), sparse=True)
+        dense1 = tf.keras.layers.Dense(self.hls, activation="relu")(inp)
+        output_layer = tf.keras.layers.Dense(self.num_classes)
+        out = output_layer(dense1)
+
+        self.full_model = tf.keras.Model(inp, out)
+        self.half_model = tf.keras.Model(inp, dense1)
+
+        self.out_weights = output_layer.weights[0]
+        self.out_bias = output_layer.bias
+
+        self.train_loss = SampledSoftmaxLoss(self.num_classes, num_sampled).loss